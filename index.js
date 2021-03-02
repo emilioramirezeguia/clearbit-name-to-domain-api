1c1894f575877268cd1bfc869e928fea3bad3ab0
Emilio Ramirez <emilioramirezeguia@gmail.com>
2021-03-02 13:45:06 -0800

    Removes duplicate values

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const fs = require("fs");
 const parse = require("csv-parse");
+const _ = require("lodash");
 
-let parser = parse({ columns: true }, (err, companies) => {
-  console.log(companies);
+// remove duplicate values
+function removeDuplicates(arr) {
+  let cleaned = [];
+
+  arr.forEach((item) => {
+    let unique = true;
+
+    cleaned.forEach((item2) => {
+      if (_.isEqual(item, item2)) {
+        unique = false;
+      }
+    });
+
+    if (unique) {
+      let capitalizedName = _.capitalize(item);
+      console.log(capitalizedName);
+      cleaned.push(item);
+    }
+  });
+
+  return cleaned;
+}
+
+// creates readable stream
+let parser = parse({ columns: true }, async (err, companies) => {
+  let uniqueCompanies = await removeDuplicates(companies);
+  console.log(uniqueCompanies);
 });
 
 // take csv file and parse it
